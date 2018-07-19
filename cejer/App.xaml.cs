4c778e5c562c9b04d36b83829c74c03d1e809505
Ruperto <ru.bugueno@gmail.com>
2018-07-19 00:14:01 -0400

    Initial commit

---
+++ cejer/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace cejer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Xamarin.Forms;
+    using Views;
+    public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new InicioPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
