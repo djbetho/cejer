a99d37385abcdd582145e93ebf1ed618b776e438
Ruperto <ru.bugueno@gmail.com>
2018-07-19 22:14:19 -0400

    Accion Button

--- cejer/ViewModels/InicioViewModels.cs
+++ cejer/ViewModels/InicioViewModels.cs
@@ -1,25 +1,43 @@
 ﻿
 
 
+
 namespace cejer.ViewModels
 {
+    using Android.App;
+
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
     public class InicioViewModels
     {
         #region Command
 
-            public ICommand Comprar
+         public ICommand ComprarCommand
         {
-            get;
-            set;
+            get
+            {
+                return new RelayCommand(Comprar);
+            }
+            
+        }
+        private async void Comprar()
+        {
+           
+            await Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Error",
+                "Text",
+                "Accept");
+            return;
         }
 
             public ICommand Pasajes
         { 
             get; 
-            set; 
+             
         }
         #endregion
     }
+
+  
 }
